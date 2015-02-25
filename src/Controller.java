b3355e0dbde8d7dca05af0e0a0184a45649467b8
Sebastian Quek <sebq13@gmail.com>
2015-02-25 21:00:48 +0800

    add Controller.java

---
+++ src/Controller.java
@@ -0,0 +1,17 @@
+public class Controller {
+    
+    public Controller(String[] args) {
+
+    }
+
+    public boolean isTimeToExit() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public String execute(String userInput) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
