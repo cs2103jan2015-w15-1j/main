b6f2294b6104602ce9cf3563177ee2549e244df0
adam <adam@u.nus.edu>
2015-02-27 23:34:24 +0800

    Update skeleton of Task class

--- src/Task.java
+++ src/Task.java
@@ -1,13 +1,37 @@
 /**
- * Created by mx on 27/2/15.
+ * Let us standardize what kind of keywords we accept for commands for V0.1
+ * 
+ * Floating task : no keywords required
+ * 
+ * Deadline task: "by" followed by a number representing day and the short-form name for month
+ *                (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)
+ * 
+ * Timed-task: "at" followed by two 24-hour notation representing start and end, "on" followed by number
+ * 			   representing day and the short-form name for month
+ *			   (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)
  */
+
+
 public class Task {
+	
+	private static String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+	
+	private String info;
+	private String taskType;
+	private boolean isCompleted;
+	private int day;
+	private int month;
+	
+	public Task (String information) {		
+		// need to process the information to check whether it is floating or not
+		
+		// if got "by" and any month from the array = deadline task
+		// if got "at", "on" and any month from the array = timed-task
+		// else it is floating
 
-    public Task() {
-
-    }
-
-    // Public getters
-
+		if (information.contains(" by ")) {
+			
+		}
+	}
 
 }
