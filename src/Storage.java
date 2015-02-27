e5d5f256251bf3e01c287ddfa64b2070b1082566
Ck91 <chenzhengkai91@gmail.com>
2015-02-27 22:43:33 +0800

    renamed Tasks to Task

--- src/Storage.java
+++ src/Storage.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 public class Storage {
-	public String writeTasksToFile(ArrayList<Tasks> input) {
+	public String writeTasksToFile(ArrayList<Task> input) {
 		return null;
 	}
 	
-	public ArrayList<Tasks> getTasksFromFile() {
+	public ArrayList<Task> getTasksFromFile() {
 		return null;
 	}
 }
