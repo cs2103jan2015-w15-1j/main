deed43539ebbe8c0a6ee50a5ecc7e33d2f07c861
MX Chai <mingxuan@live.com.sg>
2015-02-27 23:10:17 +0800

    Added StorageStub

---
+++ src/StorageStub.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class StorageStub {
+    public String writeTasksToFile(ArrayList<Task> input) {
+        return null;
+    }
+
+    public ArrayList<Task> getTasksFromFile() {
+        return null;
+    }
+}
--- src/Task.java
+++ src/Task.java
@@ -3,8 +3,11 @@
  */
 public class Task {
 
+    public Task() {
+
+    }
 
     // Public getters
-    String
+
 
 }
