607048944c75d0b95ed0bc2ec59922a6a858efd7
Muhammad Adam <splitfire_1992@hotmail.com>
2015-02-28 03:04:50 +0800

    Updated test cases

--- src/TaskTest.java
+++ src/TaskTest.java
@@ -1,19 +1,27 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
 public class TaskTest {
 
 	@Test
-	public void testIsDeadline() {
+	public void testIsDeadlineValid() {
 		Task testingTask = new Task("do homework by Jan");
 		assertTrue(testingTask.isDeadline());
 		
 		testingTask = new Task("do homework by Dec");
 		assertTrue(testingTask.isDeadline());
 		
-		testingTask = new Task("do homework by March");
+		testingTask = new Task("something something by Apr");
+		assertTrue(testingTask.isDeadline());
+	}
+	
+	@Test
+	public void testIsDeadlineInvalid() {
+		Task testingTask = new Task("do homework by March");
 		assertFalse(testingTask.isDeadline());
 		
 		testingTask = new Task("do homework by today");
@@ -24,5 +32,33 @@ public class TaskTest {
 		
 		testingTask = new Task("by Oct do homework");
 		assertFalse(testingTask.isDeadline());
+		
+		testingTask = new Task("do what I want");
+		assertFalse(testingTask.isDeadline());
+	}
+	
+	@Test
+	public void testIsTimedTaskHelperValid() {
+		Task testingTask = new Task("random gibberish");
+		ArrayList<String> testList = new ArrayList<String>();
+		testList.add("at");
+		testList.add("1200-1400");
+		testList.add("on");
+		testList.add("Apr");
+		assertTrue(testingTask.isTimedTaskHelper(testList));
+		
+		testList = new ArrayList<String>();
+		testList.add("at");
+		testList.add("10-20");
+		testList.add("on");
+		testList.add("Apr");
+		assertTrue(testingTask.isTimedTaskHelper(testList));
+		
+		testList = new ArrayList<String>();
+		testList.add("at");
+		testList.add("102032-12312314");
+		testList.add("on");
+		testList.add("Apr");
+		assertTrue(testingTask.isTimedTaskHelper(testList));
 	}
 }
