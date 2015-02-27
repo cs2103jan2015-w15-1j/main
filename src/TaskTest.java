21a346568e32689f2ed60efc2c0ef79995e24459
Muhammad Adam <splitfire_1992@hotmail.com>
2015-02-28 03:12:16 +0800

    Updated test cases

--- src/TaskTest.java
+++ src/TaskTest.java
@@ -37,6 +37,8 @@ public class TaskTest {
 		assertFalse(testingTask.isDeadline());
 	}
 	
+	// Note that this method wont check the the first index of the array
+	// The main "isTimedTask" helper will handle that
 	@Test
 	public void testIsTimedTaskHelperValid() {
 		Task testingTask = new Task("random gibberish");
@@ -60,5 +62,38 @@ public class TaskTest {
 		testList.add("on");
 		testList.add("Apr");
 		assertTrue(testingTask.isTimedTaskHelper(testList));
+	}
+	
+	@Test
+	public void testIsTimedTaskHelperInvalid() {
+		Task testingTask = new Task("random gibberish");
+		ArrayList<String> testList = new ArrayList<String>();
+		testList.add("at");
+		testList.add("1200-1400");
+		testList.add("on");
+		testList.add("April");
+		assertFalse(testingTask.isTimedTaskHelper(testList));
+		
+		testList = new ArrayList<String>();
+		testList.add("at");
+		testList.add("12pm-2pm");
+		testList.add("on");
+		testList.add("Apr");
+		assertFalse(testingTask.isTimedTaskHelper(testList));
+		
+		testList = new ArrayList<String>();
+		testList.add("at");
+		testList.add("1200-1400");
+		testList.add("by");
+		testList.add("Apr");
+		assertFalse(testingTask.isTimedTaskHelper(testList));
+		
+		testList = new ArrayList<String>();
+		testList.add("at");
+		testList.add("156432-12344");
+		testList.add("on");
+		testList.add("apr");
+		assertFalse(testingTask.isTimedTaskHelper(testList));
+		
 	}
 }
