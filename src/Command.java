a9339ee4f0dc0b83790d8354d69a8e01ccb2f91a
Sebastian Quek <sebq13@gmail.com>
2015-02-27 23:16:22 +0800

    add Command.java

---
+++ src/Command.java
@@ -0,0 +1,18 @@
+
+public class Command {
+    public static enum Type {
+                               
+    };
+    
+    public Command() {
+        
+    }
+    
+    public Type getCommandType() {
+        return null;
+    }
+    
+    public String getArguments() {
+        return null;
+    }
+}
