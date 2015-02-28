77350b3cca01fff0752620b303b6e6580ea3c338
Muhammad Adam <splitfire_1992@hotmail.com>
2015-02-28 08:03:00 +0800

    Now support day AND month

--- src/Task.java
+++ src/Task.java
@@ -9,12 +9,12 @@ import java.util.Arrays;
  * 
  * Deadline task: "by" followed by a number representing day and the short-form name for month
  *                (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)
- *                Example: "do assignment by Mar"
+ *                Example: "do assignment by 23 Mar"
  * 
  * Timed-task:    "at" followed by two 24-hour notation representing start and end (seperated by "-" without spacing,
  * 				  "on" followed by number representing day and the short-form name for month
  *			      (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)
- *                Example: "attend meeting at 1200-1400 on Apr"
+ *                Example: "attend meeting at 1200-1400 on 20 Apr"
  */
 
 public class Task {
@@ -30,6 +30,7 @@ public class Task {
 	
 	public Task (String information) {	
 		info = information;
+		isCompleted = false;
 		// need to process the information to check whether it is floating or not
 		
 		// if got "by" and any month from the array = deadline task
@@ -42,48 +43,57 @@ public class Task {
 		ArrayList<String> stringList = new ArrayList<String>(Arrays.asList(stringArr));
 		int arrayLength = stringList.size();
 		if (0 < stringList.lastIndexOf("by")) {
-			if (stringList.lastIndexOf("by")+1 == arrayLength-1) {
-				if (monthsArray.contains(stringList.get(stringList.lastIndexOf("by")+1))) {
-					return true;
-				}
-			}
+			return isDeadlineHelper(stringList.subList(stringList.lastIndexOf("by"), arrayLength));
 		}
 		return false;
 	}
 	
-	public boolean isTimedTask() {
+	private boolean isDeadlineHelper(List<String> list) {
+		if (list.size() != 3) {
+			return false;
+		} try {
+			Integer.parseInt(list.get(1));
+		} catch (NumberFormatException e) {
+			return false;
+		} if (!monthsArray.contains(list.get(2))) {
+			return false;
+		} 
+		return true;
+	}
+	
+	public boolean isTimed() {
 		String[] stringArr = info.split(" ");
 		ArrayList<String> stringList = new ArrayList<String>(Arrays.asList(stringArr));
 		int arrayLength = stringList.size();
 		if (0 < stringList.lastIndexOf("at")) {
-			return isTimedTaskHelper(stringList.subList(stringList.lastIndexOf("at"), arrayLength));
+			return isTimedHelper(stringList.subList(stringList.lastIndexOf("at"), arrayLength));
 		}
 		return false;
 	}
 	
-	public boolean isTimedTaskHelper(List<String> list) {
-		boolean answer = true;
-		if (list.size() != 4) {
-			answer = false;
+	private boolean isTimedHelper(List<String> list) {
+		if (list.size() != 5) {
+			return false;
 		}
 		String[] array = list.get(1).split("-");
 		if (array.length != 2) {
-			answer = false;
-		}
-		try {
+			return false;
+		} try {
 			Integer.parseInt(array[0]);
 			Integer.parseInt(array[1]);
 		} catch (NumberFormatException e) {
-			answer = false;
+			return false;
 		} catch (ArrayIndexOutOfBoundsException e) {
-			answer = false;
+			return false;
+		} if (!list.get(2).equals("on")) {
+			return false;
+		} try {
+			Integer.parseInt(list.get(3));
+		} catch (NumberFormatException e) {
+			return false;
+		} if (!monthsArray.contains(list.get(4))) {
+			return false;
 		}
-		if (!list.get(2).equals("on")) {
-			answer = false;
-		}
-		if (!monthsArray.contains(list.get(3))) {
-			answer = false;
-		}
-		return answer;
+		return true;
 	}
 }
