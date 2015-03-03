6b7bebb4038b145c668c577b1a76854e905e36b5
Ck91 <chenzhengkai91@gmail.com>
2015-03-03 12:54:12 +0800

    Updated Storage constructor

--- src/Storage.java
+++ src/Storage.java
@@ -1,14 +1,22 @@
+import java.io.File;
 import java.util.ArrayList;
 
 public class Storage {
+    private File saveFile;
+
     public Storage(String fileName) {
+        saveFile = new File(fileName);
+        createMissingFile(saveFile);
     }
-    
-	public String writeTasksToFile(ArrayList<Task> input) {
-		return null;
-	}
-	
-	public ArrayList<Task> getTasksFromFile() {
-		return null;
-	}
+
+    private void createMissingFile(File saveFile2) {
+    }
+
+    public String writeTasksToFile(ArrayList<Task> input) {
+        return null;
+    }
+
+    public ArrayList<Task> getTasksFromFile() {
+        return null;
+    }
 }
