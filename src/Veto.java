2f1dbd3052d73e80f17062117eafb9ad1bfa532f
Sebastian Quek <sebq13@gmail.com>
2015-03-03 23:03:53 +0800

    Veto now has text prompts.

--- src/Veto.java
+++ src/Veto.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 
 public class Veto {
+    private static final String MESSAGE_COMMAND_PROMPT = "Command: ";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Controller controller = new Controller(args);
+        showLine(controller.getWelcomeMessage());
 
         while (!controller.isTimeToExit()) {
+            show(MESSAGE_COMMAND_PROMPT);
             String userInput = scanner.nextLine();
             String feedback = controller.executeCommand(userInput);
             showLine(feedback);
@@ -16,8 +19,12 @@ public class Veto {
         scanner.close();
     }
 
-    private static void showLine(String feedback) {
-        System.out.println(feedback);
+    public static void show(String text) {
+        System.out.print(text);
+    }
+
+    private static void showLine(String text) {
+        System.out.println(text);
     }
 
 }
