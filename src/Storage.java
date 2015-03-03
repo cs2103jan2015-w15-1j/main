dc3bff87b9ec531d54346183ac71beb3dd7e0153
Ck91 <chenzhengkai91@gmail.com>
2015-03-03 12:50:59 +0800

    create constructor for Storage.java

--- src/Storage.java
+++ src/Storage.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
 public class Storage {
+    public Storage(String fileName) {
+    }
+    
 	public String writeTasksToFile(ArrayList<Task> input) {
 		return null;
 	}
