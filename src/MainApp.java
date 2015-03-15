0232f13dfe9202f705c516c4d5c9eef88a055261
MX Chai <mingxuan@live.com.sg>
2015-03-15 20:31:43 +0800

    Mockup of GUI

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,20 +1,65 @@
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainApp extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception{
-		Parent root = FXMLLoader.load(getClass().getResource("hola.fxml"));
-		primaryStage.setScene(new Scene(root, 300, 275));
-		primaryStage.show();
-	}
+	private Stage primaryStage;
+	private BorderPane rootLayout;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Veto");
+
+		initRootLayout();
+		showTaskOverview();
+	}
+
+
+
+	/**
+	 * Initializes the root layout.
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showTaskOverview() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("TaskOverview.fxml"));
+			AnchorPane personOverview = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(personOverview);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
 }
---
+++ src/TaskOverviewController.java
@@ -0,0 +1,16 @@
+/**
+ * Created by mx on 15/3/15.
+ */
+public class TaskOverviewController {
+
+    private MainApp mainApp;
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     *
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+}
