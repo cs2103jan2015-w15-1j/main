fe78b85b7ed25869e0dbf4a31be80ec93fe9c3bc
Sebastian Quek <sebq13@gmail.com>
2015-03-27 21:51:22 +0800

    Refactored updateDisplay and Fixed issue #85

--- src/main/resources/view/Display.java
+++ src/main/resources/view/Display.java
@@ -23,33 +23,30 @@ import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class Display extends AnchorPane {
+
+
     // ================================================================
     // FXML Fields
     // ================================================================
     @FXML
     private ListView<HBox> listView;
 
+
     // ================================================================
     // Non-FXML Fields
     // ================================================================
-    private String saveFileName;
-    private Storage storage;
-    private boolean timeToExit;
-
-    private ArrayList<Task> incompleteTasks;
-    private ArrayList<Task> completedTasks;
-
-    private Stack<ArrayList<Task>> previousStates;
-
-    private ObservableList<Task> displayedTasks = FXCollections.observableArrayList();
     private ObservableList<HBox> displayBoxes = FXCollections.observableArrayList();
-    private DateParser parser;
+
 
     // ================================================================
     // Constants
     // ================================================================
     private final static String TASK_OVERVIEW_LOCATION = "/view/TaskOverview.fxml";
-    private static final String MESSAGE_EMPTY = "There is currently no task.\n";
+    private static final String LABEL_FLOATING = "Floating";
+    private static final String LABEL_OVERDUE = "Overdue";
+    private static final String LABEL_TODAY = "Today";
+    private static final String LABEL_TOMORROW = "Tomorrow";
+    private static final String LABEL_OTHERS = "Everything else";
 
 
     // ================================================================
@@ -74,9 +71,6 @@ public class Display extends AnchorPane {
     // Public methods
     // ================================================================
     public void updateDisplay(ObservableList<Task> tasks) {
-        // TODO THIS METHOD NEEDS REFACTORING
-        // TODO THIS METHOD NEEDS REFACTORING
-        // TODO THIS METHOD NEEDS REFACTORING
         ArrayList<Task> listOfTasks = sortToDisplay(new ArrayList<Task>(tasks));
 
         System.out.println(listOfTasks.toString());
@@ -86,76 +80,10 @@ public class Display extends AnchorPane {
         LocalDate now = LocalDate.now();
         int i = 1;
 
-        // add first category
-        DayBox floating = new DayBox("Floating", "");
-        displayBoxes.add(floating);
-        boolean hasFloating = false;
-        for (Task t : listOfTasks) {
-            if (t.getType() == Task.Type.FLOATING) {
-                hasFloating = true;
-                displayBoxes.add(new TaskBox(i, t.getDescription()));
-                i++;
-            }
-        }
-        if (!hasFloating) {
-            floating.dim();
-        }
-
-        // add second category
-        DayBox overdue = new DayBox("Overdue", "");
-        displayBoxes.add(overdue);
-        boolean hasOverdue = false;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, d MMMM y");
-        for (Task t : listOfTasks) {
-            if (t.isOverdue()) {
-                hasOverdue = true;
-                displayBoxes.add(new TaskBox(i, t.getDescription() + " on " +
-                        t.getDate().format(formatter)));
-                i++;
-            }
-        }
-        if (!hasOverdue) {
-            overdue.dim();
-        }
-
-        // generate the dates of the 7 days from today
-        ArrayList<LocalDate> days = new ArrayList<LocalDate>();
-        days.add(now);
-        for (int j = 1; j < 7; j++) {
-            days.add(now.plusDays(j));
-        }
-
-        // formats the date for the day label, eg. Monday, Tuesday, etc
-        DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern("EEEE");
-
-        // formats the date for the date label, eg. 1 April
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d MMMM");
-
-
-        for (LocalDate day : days) {
-            DayBox label;
-            if (day.equals(now)) {
-                // special cases to show "Today" and "Tomorrow" instead
-                label = new DayBox("Today", day.format(dateFormatter));
-            } else if (day.equals(now.plusDays(1))) {
-                label = new DayBox("Tomorrow", day.format(dateFormatter));
-            } else {
-                label = new DayBox(day.format(dayFormatter),
-                        day.format(dateFormatter));
-            }
-            displayBoxes.add(label);
-            boolean hasTaskOnThisDay = false;
-            for (Task t : listOfTasks) {
-                if (t.getDate() != null && t.getDate().isEqual(day)) {
-                    hasTaskOnThisDay = true;
-                    displayBoxes.add(new TaskBox(i, t.getDescription()));
-                    i++;
-                }
-            }
-            if (!hasTaskOnThisDay) {
-                label.dim();
-            }
-        }
+        i = addFloatingTasks(listOfTasks, i);
+        i = addOverdueTasks(listOfTasks, i);
+        i = addThisWeeksTasks(listOfTasks, now, i);
+        i = addAllOtherTasks(listOfTasks, now, i);
 
         listView.setItems(displayBoxes);
     }
@@ -163,63 +91,195 @@ public class Display extends AnchorPane {
     // ================================================================
     // Logic methods
     // ================================================================
+    private int addFloatingTasks(ArrayList<Task> listOfTasks, int i) {
+        DayBox floating = new DayBox(LABEL_FLOATING, "");
+        displayBoxes.add(floating);
+
+        boolean hasFloating = false;
+
+        for (Task t : listOfTasks) {
+            if (t.getType() == Task.Type.FLOATING) {
+                hasFloating = true;
+                displayBoxes.add(new TaskBox(i, t.getDescription()));
+                i++;
+            }
+        }
+
+        if (!hasFloating) {
+            floating.dim();
+        }
+
+        return i;
+    }
+
+    private int addOverdueTasks(ArrayList<Task> listOfTasks, int i) {
+        // add second category
+        DayBox overdue = new DayBox(LABEL_OVERDUE, "");
+        displayBoxes.add(overdue);
+
+        boolean hasOverdue = false;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, d MMMM y");
+
+        for (Task t : listOfTasks) {
+            if (t.isOverdue()) {
+                hasOverdue = true;
+                displayBoxes.add(new TaskBox(i, t.getDescription() + " on " +
+                                                t.getDate().format(formatter)));
+                i++;
+            }
+        }
+
+        if (!hasOverdue) {
+            overdue.dim();
+        }
+
+        return i;
+    }
+
+    private int addThisWeeksTasks(ArrayList<Task> listOfTasks,
+                                  LocalDate now,
+                                  int i) {
+        // generate the dates of the 7 days from today
+        ArrayList<LocalDate> days = generateDaysOfWeek(now);
+
+        // formats the date for the day label, eg. Monday, Tuesday, etc
+        DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern("EEEE");
+
+        // formats the date for the date label, eg. 1 April
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d MMMM");
+
+        for (LocalDate day : days) {
+            DayBox label = generateDayLabel(now, dayFormatter, dateFormatter,
+                                            day);
+            displayBoxes.add(label);
+
+            boolean hasTaskOnThisDay = false;
+
+            for (Task t : listOfTasks) {
+                if (day.equals(t.getDate())) {
+                    hasTaskOnThisDay = true;
+                    displayBoxes.add(new TaskBox(i, t.getDescription()));
+                    i++;
+                }
+            }
+
+            if (!hasTaskOnThisDay) {
+                label.dim();
+            }
+        }
+        return i;
+    }
+
+    private DayBox generateDayLabel(LocalDate now,
+                                    DateTimeFormatter dayFormatter,
+                                    DateTimeFormatter dateFormatter,
+                                    LocalDate day) {
+        DayBox label;
+        // special cases to show "Today" and "Tomorrow" instead of day
+        if (day.equals(now)) {
+            label = new DayBox(LABEL_TODAY, day.format(dateFormatter));
+        } else if (day.equals(now.plusDays(1))) {
+            label = new DayBox(LABEL_TOMORROW, day.format(dateFormatter));
+        } else {
+            label = new DayBox(day.format(dayFormatter),
+                               day.format(dateFormatter));
+        }
+        return label;
+    }
+
+    private ArrayList<LocalDate> generateDaysOfWeek(LocalDate now) {
+        ArrayList<LocalDate> days = new ArrayList<LocalDate>();
+        days.add(now);
+        for (int j = 1; j < 7; j++) {
+            days.add(now.plusDays(j));
+        }
+        return days;
+    }
+
+    private int addAllOtherTasks(ArrayList<Task> listOfTasks,
+                                 LocalDate now,
+                                 int i) {
+        DayBox otherTasks = new DayBox(LABEL_OTHERS, "");
+        displayBoxes.add(otherTasks);
+
+        boolean hasOtherTasks = false;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, d MMMM y");
+        LocalDate dayOneWeekFromNow = now.plusWeeks(1);
+
+        for (Task t : listOfTasks) {
+            if (t.getDate() != null && dayOneWeekFromNow.isBefore(t.getDate())) {
+                hasOtherTasks = true;
+                displayBoxes.add(new TaskBox(i, t.getDescription() + " on " +
+                                                t.getDate().format(formatter)));
+                i++;
+            }
+        }
+
+        if (!hasOtherTasks) {
+            otherTasks.dim();
+        }
+
+        return i;
+    }
 
     private ArrayList<Task> sortToDisplay(ArrayList<Task> list) {
-    	ArrayList<Task> overdueTasks = new ArrayList<Task>();
-    	ArrayList<Task> floatingTasks = new ArrayList<Task>();
-    	ArrayList<Task> notOverdueTasks = new ArrayList<Task>();
-    	ArrayList<Task> finalList = new ArrayList<Task>();
+        ArrayList<Task> overdueTasks = new ArrayList<Task>();
+        ArrayList<Task> floatingTasks = new ArrayList<Task>();
+        ArrayList<Task> notOverdueTasks = new ArrayList<Task>();
+        ArrayList<Task> finalList = new ArrayList<Task>();
 
-    	// Separate the floating, overdue and pending
-    	for (Task task: list) {
-    		if (task.getType() == Task.Type.FLOATING) {
-    			floatingTasks.add(task);
-    		} else if (task.isOverdue()) {
-    			overdueTasks.add(task);
-    		} else {
-    			notOverdueTasks.add(task);
-    		}
-    	}
+        // Separate the floating, overdue and pending
+        for (Task task : list) {
+            if (task.getType() == Task.Type.FLOATING) {
+                floatingTasks.add(task);
+            } else if (task.isOverdue()) {
+                overdueTasks.add(task);
+            } else {
+                notOverdueTasks.add(task);
+            }
+        }
 
-    	// Sort according to what we discussed
-    	overdueTasks = sortByDateAndType(overdueTasks);
-    	notOverdueTasks = sortByDateAndType(notOverdueTasks);
+        // Sort according to what we discussed
+        overdueTasks = sortByDateAndType(overdueTasks);
+        notOverdueTasks = sortByDateAndType(notOverdueTasks);
 
-    	finalList.addAll(floatingTasks);
-    	finalList.addAll(overdueTasks);
-    	finalList.addAll(notOverdueTasks);
+        finalList.addAll(floatingTasks);
+        finalList.addAll(overdueTasks);
+        finalList.addAll(notOverdueTasks);
 
         return finalList;
     }
 
     private ArrayList<Task> sortByDateAndType(ArrayList<Task> list) {
-    	ArrayList<Task> output = new ArrayList<Task>();
+        ArrayList<Task> output = new ArrayList<Task>();
 
-    	for (Task task: list) {
-    		if (output.size() == 0) {
-    			output.add(task);
-    		} else {
-    			for (Task something: output) {
-    				if (task.getDate().isBefore(something.getDate())) {
-    					output.add(output.indexOf(something), task);
-    					break;
-    				} else if (task.getDate().isEqual(something.getDate())) {
-    					if (something.getType() == Task.Type.TIMED && task.getType() == Task.Type.DEADLINE) {
-    						output.add(output.indexOf(something), task);
-    						break;
-    					}
-    				}
-    			}
-    			output.add(task);
-    		}
-    	}
-    	return output;
+        for (Task task : list) {
+            if (output.size() == 0) {
+                output.add(task);
+            } else {
+                for (Task something : output) {
+                    if (task.getDate().isBefore(something.getDate())) {
+                        output.add(output.indexOf(something), task);
+                        break;
+                    } else if (task.getDate().isEqual(something.getDate())) {
+                        if (something.getType() == Task.Type.TIMED &&
+                            task.getType() == Task.Type.DEADLINE) {
+                            output.add(output.indexOf(something), task);
+                            break;
+                        }
+                    }
+                }
+                output.add(task);
+            }
+        }
+        return output;
     }
 
     // ================================================================
     // Utility methods
     // ================================================================
-    private ArrayList<Task> concatenateTasks(ArrayList<Task> first, ArrayList<Task> second) {
+    private ArrayList<Task> concatenateTasks(ArrayList<Task> first,
+                                             ArrayList<Task> second) {
         ArrayList<Task> output = new ArrayList<Task>();
         output.addAll(first);
         output.addAll(second);
