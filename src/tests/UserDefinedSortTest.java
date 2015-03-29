ada73536d4e4b67e6588b9284e732a86075f8e53
Muhammad Adam <splitfire_1992@hotmail.com>
2015-03-29 17:00:02 +0800

    Create Test Unit for UserDefinedSort class

---
+++ src/tests/UserDefinedSortTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.ArrayList;
+import main.java.UserDefinedSort;
+import main.java.Task;
+
+public class UserDefinedSortTest {
+
+	@Test
+	public void TestTypeSort() {
+		
+	}
+
+}
