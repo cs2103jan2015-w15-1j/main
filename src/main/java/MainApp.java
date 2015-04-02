0940c77348f5e2d7d5e80e3b7e81c8f268d40084
Sebastian Quek <sebq13@gmail.com>
2015-04-02 16:13:51 +0800

    Fonts and icon should load properly now. Implemented minimum height and width for window.

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -2,18 +2,22 @@ package main.java;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import main.resources.view.RootLayoutController;
 import main.resources.view.Display;
 
 public class MainApp extends Application {
-	// ================================================================
+
+    // ================================================================
 	// Fields
 	// ================================================================
 	private Stage primaryStage;
 	private Display display;
 	private RootLayoutController rootLayoutController;
 	private Controller controller;
+    private static final int STAGE_MINIMUM_HEIGHT = 650;
+    private static final int STAGE_MINIMUM_WIDTH = 650;
 
 	// ================================================================
 	// Methods
@@ -25,8 +29,11 @@ public class MainApp extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		this.primaryStage = primaryStage;
+		this.primaryStage.getIcons().add(new Image("/images/icon.png")); 
 		this.primaryStage.setTitle("Veto");  // --> This represent the name on the Windows Title Bar
-
+		this.primaryStage.setMinWidth(STAGE_MINIMUM_WIDTH);
+		this.primaryStage.setMinHeight(STAGE_MINIMUM_HEIGHT);
+		
 		initRootLayout();
 		initDisplay();
 		initController();
