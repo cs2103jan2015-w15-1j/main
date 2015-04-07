4cea84481ec4b19b96da01b8150fa61e60798ca7
Muhammad Adam <splitfire_1992@hotmail.com>
2015-04-08 03:10:31 +0800

    Proper testing of UDS class and its subs

--- src/tests/UserDefinedSortTest.java
+++ src/tests/UserDefinedSortTest.java
@@ -30,6 +30,19 @@ public class UserDefinedSortTest {
 	}
 
 	@Test
+	public void TestUDSContainer() {
+		ArrayList<Task> sampleList = new ArrayList<Task>();
+		sampleList.add(createTask("first"));
+		sampleList.add(createTask("second"));
+		sampleList.add(createTask("third"));
+		
+		UserDefinedSort testContainer = new UserDefinedSort(sampleList);
+		
+		assertEquals(sampleList, testContainer.getList());
+		assertEquals(sampleList, testContainer.executeSort());
+	}
+	
+	@Test
 	public void TestTypeSort() {
 		 
 		list = new ArrayList<Task>();
@@ -146,91 +159,5 @@ public class UserDefinedSortTest {
 	    assertEquals(list.get(0), taskDeadline);
 	    assertEquals(list.get(1), taskFloating);
 	    assertEquals(list.get(2), taskTimed);
-	}
-	
-	@Test
-	public void TestAllCombined() {
-		
-		list = new ArrayList<Task>();
-		uds = new UserDefinedSort(list);
-		
-		Task taskTimed1 = createTask("do homework 2pm to 3pm on friday");
-		Task taskDeadline1 = createTask("buy milk tomorrow");
-		Task taskFloating1 = createTask("do homework");
-		Task taskTimed2 = createTask("watch movie with friends today 5pm to 8pm");
-		Task taskDeadline2 = createTask("buy present today");
-		Task taskFloating2 = createTask("go to school");
-		Task taskTimed3 = createTask("exam from 8am to 10am on Thursday");
-		Task taskDeadline3 = createTask("teach tuition yesterday");
-		Task taskFloating3 = createTask("remember to drink water");
-		Task taskTimed4 = createTask("got meeting yesterday at 10am to 12pm");
-		Task taskDeadline4 = createTask("buy cereal 28 march");
-		Task taskFloating4 = createTask("float to swim");
-		Task taskTimed5 = createTask("IPPT to attend 23 april 1pm to 2pm");
-		Task taskDeadline5 = createTask("do something tomorrow");
-		Task taskFloating5 = createTask("do shit");
-		
-		taskTimed2.markAsCompleted();
-		taskDeadline1.markAsCompleted();
-		taskFloating5.markAsCompleted();
-		taskFloating4.markAsCompleted();
-		taskDeadline3.markAsCompleted();
-		taskTimed5.markAsCompleted();
-		
-		list.add(taskTimed1);        // do homework 2pm to 3pm 3 days later
-	    list.add(taskDeadline1);     // buy milk tomorrow (COMPLETED)
-	    list.add(taskFloating1);     // do homework\
-	    list.add(taskTimed2);        // watch movie with friends today 5pm to 8pm (COMPLETED)
-	    list.add(taskDeadline2);     // buy present today
-	    list.add(taskFloating2);     // go to school\
-	    list.add(taskTimed3);        // exam from 8am to 10am next week
-	    list.add(taskDeadline3);     // teach tuition 2 days later (COMPLETED)
-	    list.add(taskFloating3);     // remember to drink water\
-	    list.add(taskTimed4);        // got meeting yesterday at 10am to 12pm
-	    list.add(taskDeadline4);     // buy cereal 2 days ago
-	    list.add(taskFloating4);     // float to swim (COMPLETED)
-	    list.add(taskTimed5);        // IPPT to attend next week 1pm to 2pm (COMPLETED)
-	    list.add(taskDeadline5);     // do something next 2 days
-	    list.add(taskFloating5);     // do shit (COMPLETED)
-	    
-	    uds.addComparator(new SortType());
-	    uds.addComparator(new SortOverdue());
-	    uds.addComparator(new SortDate());
-	    uds.addComparator(new SortIncomplete());
-		
-	    assertEquals(list.get(0), taskTimed1);
-	    assertEquals(list.get(1), taskDeadline1);
-	    assertEquals(list.get(2), taskFloating1);
-	    assertEquals(list.get(3), taskTimed2);
-	    assertEquals(list.get(4), taskDeadline2);
-	    assertEquals(list.get(5), taskFloating2);
-	    assertEquals(list.get(6), taskTimed3);
-	    assertEquals(list.get(7), taskDeadline3);
-	    assertEquals(list.get(8), taskFloating3);
-	    assertEquals(list.get(9), taskTimed4);
-	    assertEquals(list.get(10), taskDeadline4);
-	    assertEquals(list.get(11), taskFloating4);
-	    assertEquals(list.get(12), taskTimed5);
-	    assertEquals(list.get(13), taskDeadline5);
-	    assertEquals(list.get(14), taskFloating5);
-	    
-	    uds.executeSort();
-	    
-	    assertEquals(list.get(0), taskFloating1);
-	    assertEquals(list.get(1), taskFloating2);
-	    assertEquals(list.get(2), taskFloating3);
-	    assertEquals(list.get(3), taskTimed2);
-	    assertEquals(list.get(4), taskDeadline2);
-	    assertEquals(list.get(5), taskFloating2);
-	    assertEquals(list.get(6), taskTimed3);
-	    assertEquals(list.get(7), taskDeadline3);
-	    assertEquals(list.get(8), taskFloating3);
-	    assertEquals(list.get(9), taskTimed4);
-	    assertEquals(list.get(10), taskDeadline4);
-	    assertEquals(list.get(11), taskFloating4);
-	    assertEquals(list.get(12), taskTimed5);
-	    assertEquals(list.get(13), taskDeadline5);
-	    assertEquals(list.get(14), taskFloating5);
-		
 	}
 }
