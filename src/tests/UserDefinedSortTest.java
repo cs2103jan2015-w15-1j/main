df18cd66a2aea945ba2468ee409996c967fa5724
Muhammad Adam <splitfire_1992@hotmail.com>
2015-04-11 18:32:57 +0800

    Update UDS test.

--- src/tests/UserDefinedSortTest.java
+++ src/tests/UserDefinedSortTest.java
@@ -1,12 +1,16 @@
 package tests;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+
 import main.java.SortDate;
 import main.java.SortIncomplete;
 import main.java.SortOverdue;
+import main.java.SortTime;
 import main.java.UserDefinedSort;
 import main.java.Task;
 import main.java.DateParser;
@@ -49,27 +53,31 @@ public class UserDefinedSortTest {
 		list = new ArrayList<Task>();
 		uds = new UserDefinedSort(list);
 		
-	    Task taskDeadline = createTask("buy milk tomorrow");
+	    Task taskDeadlineNoTime = createTask("buy milk tomorrow");
+	    Task taskDeadlineWithTime = createTask("buy cereal tomorrow by 9am");
 	    Task taskFloating = createTask("do homework");
 	    Task taskTimed = createTask("do assignment today 2pm to 3pm");
 	   
 	    list.add(taskTimed);
-	    list.add(taskDeadline);
+	    list.add(taskDeadlineWithTime);
+	    list.add(taskDeadlineNoTime);
 	    list.add(taskFloating);
 	    
 	    uds.addComparator(new SortType());
 	    
 	    // Test before sort
 	    assertEquals(list.get(0), taskTimed);
-	    assertEquals(list.get(1), taskDeadline);
-	    assertEquals(list.get(2), taskFloating);
+	    assertEquals(list.get(1), taskDeadlineWithTime);
+	    assertEquals(list.get(2), taskDeadlineNoTime);
+	    assertEquals(list.get(3), taskFloating);
 	    
 	    uds.executeSort();
 	    
 	    // Test after sort
 	    assertEquals(list.get(0), taskFloating);
-	    assertEquals(list.get(1), taskDeadline);
-	    assertEquals(list.get(2), taskTimed);
+	    assertEquals(list.get(1), taskTimed);
+	    assertEquals(list.get(2), taskDeadlineWithTime);
+	    assertEquals(list.get(3), taskDeadlineNoTime);
 	}
 	
 	@Test
@@ -78,27 +86,32 @@ public class UserDefinedSortTest {
 		list = new ArrayList<Task>();
 		uds = new UserDefinedSort(list);
 		
-		Task taskTimed = createTask("do assignment tomorrow");
-		Task taskDeadline = createTask("buy milk today");
-		Task taskFloating = createTask("do homework");
+		Task taskDeadlineNoTime = createTask("buy milk tomorrow");
+	    Task taskDeadlineWithTime = createTask("buy cereal tomorrow by 9am");
+	    Task taskFloating = createTask("do homework");
+	    Task taskTimed = createTask("do assignment today 2pm to 3pm");
 
 	    list.add(taskTimed);
-	    list.add(taskDeadline);
+	    list.add(taskDeadlineNoTime);
+	    list.add(taskDeadlineWithTime);
 	    list.add(taskFloating);
 	    
 	    uds.addComparator(new SortDate());
 	    
 	    // Test before sort
 	    assertEquals(list.get(0), taskTimed);
-	    assertEquals(list.get(1), taskDeadline);
-	    assertEquals(list.get(2), taskFloating);
+	    assertEquals(list.get(1), taskDeadlineNoTime);
+	    assertEquals(list.get(2), taskDeadlineWithTime);
+	    assertEquals(list.get(3), taskFloating);
 	      
 	    uds.executeSort();
 	    
 	    // Test after sort
 	    assertEquals(list.get(0), taskFloating);
-	    assertEquals(list.get(1), taskDeadline);
-	    assertEquals(list.get(2), taskTimed);
+	    assertEquals(list.get(1), taskTimed);
+	    assertEquals(list.get(2), taskDeadlineNoTime);
+	    assertEquals(list.get(3), taskDeadlineWithTime);
+	    
 	}
 	
 	@Test
@@ -107,27 +120,31 @@ public class UserDefinedSortTest {
 		list = new ArrayList<Task>();
 		uds = new UserDefinedSort(list);
 		
-		Task taskTimed = createTask("do assignment today 2pm to 3pm");
-		Task taskDeadline = createTask("buy milk yesterday");
-		Task taskFloating = createTask("do homework");
+		Task taskDeadlineNoTime = createTask("buy milk tomorrow");
+	    Task taskDeadlineWithTime = createTask("buy cereal by 9am");
+	    Task taskFloating = createTask("do homework");
+	    Task taskTimed = createTask("do assignment 9 march 2pm to 3pm");
 	    
+	    list.add(taskDeadlineWithTime);
 	    list.add(taskTimed);
-	    list.add(taskDeadline);
+	    list.add(taskDeadlineNoTime);
 	    list.add(taskFloating);
 	    
 	    uds.addComparator(new SortOverdue());
 	    
 	    // Test before sort
-	    assertEquals(list.get(0), taskTimed);
-	    assertEquals(list.get(1), taskDeadline);
-	    assertEquals(list.get(2), taskFloating);
+	    assertEquals(list.get(0), taskDeadlineWithTime);
+	    assertEquals(list.get(1), taskTimed);
+	    assertEquals(list.get(2), taskDeadlineNoTime);
+	    assertEquals(list.get(3), taskFloating);
 	    
 	    uds.executeSort();
 	    
 	    // Test after sort
-	    assertEquals(list.get(0), taskDeadline);
-	    assertEquals(list.get(1), taskTimed);
-	    assertEquals(list.get(2), taskFloating);
+	    assertEquals(list.get(0), taskTimed);
+	    assertEquals(list.get(1), taskDeadlineWithTime);
+	    assertEquals(list.get(2), taskDeadlineNoTime);
+	    assertEquals(list.get(3), taskFloating);
 	}
 	
 	@Test
@@ -136,29 +153,119 @@ public class UserDefinedSortTest {
 		list = new ArrayList<Task>();
 		uds = new UserDefinedSort(list);
 		
-		Task taskTimed = createTask("do assignment today 2pm to 3pm");
-		Task taskDeadline = createTask("buy milk yesterday");
-		Task taskFloating = createTask("do homework");
+		Task taskDeadlineNoTime = createTask("buy milk tomorrow");
+	    Task taskDeadlineWithTime = createTask("buy cereal by 9am");
+	    Task taskFloating = createTask("do homework");
+	    Task taskTimed = createTask("do assignment 9 march 2pm to 3pm");
 
-	    // Make 1 task completed
+	    // Make 2 tasks completed
 	    taskTimed.markAsCompleted();
+	    taskFloating.markAsCompleted();
 	    
+	    list.add(taskDeadlineNoTime);
 	    list.add(taskTimed);
-	    list.add(taskDeadline);
+	    list.add(taskDeadlineWithTime);
 	    list.add(taskFloating);
 	    
 	    uds.addComparator(new SortIncomplete());
 	    
 	    // Test before sort
-	    assertEquals(list.get(0), taskTimed);
-	    assertEquals(list.get(1), taskDeadline);
-	    assertEquals(list.get(2), taskFloating);
+	    assertEquals(list.get(0), taskDeadlineNoTime);
+	    assertEquals(list.get(1), taskTimed);
+	    assertEquals(list.get(2), taskDeadlineWithTime);
+	    assertEquals(list.get(3), taskFloating);
 	    
 	    uds.executeSort();
 	    
 	    // Test after sort
-	    assertEquals(list.get(0), taskDeadline);
-	    assertEquals(list.get(1), taskFloating);
+	    assertEquals(list.get(0), taskDeadlineNoTime);
+	    assertEquals(list.get(1), taskDeadlineWithTime);
 	    assertEquals(list.get(2), taskTimed);
+	    assertEquals(list.get(3), taskFloating);
+	}
+	
+	@Test
+	public void TestTimeSort() {
+		
+		list = new ArrayList<Task>();
+		uds = new UserDefinedSort(list);
+		
+		Task taskDeadlineNoTime = createTask("buy milk tomorrow");
+	    Task taskDeadlineWithTime = createTask("buy cereal by 9am");
+	    Task taskFloating = createTask("do homework");
+	    Task taskTimed = createTask("do assignment 9 march 2pm to 3pm");
+	    
+	    list.add(taskDeadlineNoTime);
+	    list.add(taskTimed);
+	    list.add(taskDeadlineWithTime);
+	    list.add(taskFloating);
+	    
+	    uds.addComparator(new SortTime());
+	    
+	    // Test before sort
+	    assertEquals(list.get(0), taskDeadlineNoTime);
+	    assertEquals(list.get(1), taskTimed);
+	    assertEquals(list.get(2), taskDeadlineWithTime);
+	    assertEquals(list.get(3), taskFloating);
+	    
+	    uds.executeSort();
+	    
+	    // Test after sort
+	    assertEquals(list.get(0), taskDeadlineNoTime);
+	    assertEquals(list.get(1), taskFloating);
+	    assertEquals(list.get(2), taskDeadlineWithTime);
+	    assertEquals(list.get(3), taskTimed);
+	}
+	
+	@Test
+	// Two list of tasks in different order, when going through the same chain
+	// should result in same output order
+	public void TestSortChain() {
+		
+		Task taskDeadlineNoTime = createTask("buy milk tomorrow");
+	    Task taskDeadlineWithTime = createTask("buy cereal by 9am");
+	    Task taskFloating = createTask("do homework");
+	    Task taskTimed = createTask("do assignment 9 march 2pm to 3pm");
+	    
+	    taskDeadlineNoTime.markAsCompleted();
+		
+		// Test sequence 1
+		list = new ArrayList<Task>();
+		uds = new UserDefinedSort(list);
+	    
+	    list.add(taskDeadlineNoTime);
+	    list.add(taskTimed);
+	    list.add(taskDeadlineWithTime);
+	    list.add(taskFloating);
+	    
+	    uds.addComparator(new SortType());
+	    uds.addComparator(new SortTime());
+	    uds.addComparator(new SortDate());
+	    uds.addComparator(new SortOverdue());
+	    uds.addComparator(new SortIncomplete());
+	    
+	    uds.executeSort();
+	    
+	    // Test sequence 2
+	    ArrayList<Task> listCopy = new ArrayList<Task>();
+	    UserDefinedSort udsCopy = new UserDefinedSort(listCopy);
+	    
+	    listCopy.add(taskFloating);
+	    listCopy.add(taskDeadlineWithTime);
+	    listCopy.add(taskTimed);
+	    listCopy.add(taskDeadlineNoTime);
+
+	    udsCopy.addComparator(new SortType());
+	    udsCopy.addComparator(new SortTime());
+	    udsCopy.addComparator(new SortDate());
+	    udsCopy.addComparator(new SortOverdue());
+	    udsCopy.addComparator(new SortIncomplete());
+	    
+	    udsCopy.executeSort();
+	    
+	    assertEquals(list.get(0), listCopy.get(0));
+	    assertEquals(list.get(1), listCopy.get(1));
+	    assertEquals(list.get(2), listCopy.get(2));
+	    assertEquals(list.get(3), listCopy.get(3));
 	}
 }
