b0f013958ffbbf7dd6021bd64b00d16a18d72e13
Ck91 <chenzhengkai91@gmail.com>
2015-04-13 04:25:20 +0800

    Refractor (naming) & specific testing

--- src/tests/CreateTaskTest.java
+++ src/tests/CreateTaskTest.java
@@ -8,31 +8,25 @@ import org.junit.Test;
 //@author A0122393L
 public class CreateTaskTest {
     CreateTask taskCreate = CreateTask.getInstance();
-    String[] testStrings = { "do homework every 3 day",
-            "play game with friends daily",
-            "buy grocery weekly from today to 23 jul",
-            "i just want to simply do nothing",
-            "transfer money to parents monthly",
-            "consult professor every 2 weeks",
-            "create a good and helpful software yearly",
-            "find new android game weekly from tomorrow till 21 dec",
-            "go overseas for a good get away every 2 year",
-            "renew season parking for carpark every 3 month" };
+    String[] frequencyStrings = { "every 3 day", "daily",
+            "weekly from today to 23 jul", "nothing", "monthly",
+            "every 2 weeks", "yearly", "weekly from tomorrow till 21 dec",
+            "every 2 year", "every 3 month" };
 
     // -------------------------------------------------------------------
     // test getting the right rate and frequency
     // -------------------------------------------------------------------
     @Test
     public void frequencyTest() {
-        assertEquals("3 daily", CreateTask.frequencyTest(testStrings[0]));
-        assertEquals("1 daily", CreateTask.frequencyTest(testStrings[1]));
-        assertEquals("1 weekly", CreateTask.frequencyTest(testStrings[2]));
-        assertEquals("", CreateTask.frequencyTest(testStrings[3]));
-        assertEquals("1 monthly", CreateTask.frequencyTest(testStrings[4]));
-        assertEquals("2 weekly", CreateTask.frequencyTest(testStrings[5]));
-        assertEquals("1 yearly", CreateTask.frequencyTest(testStrings[6]));
-        assertEquals("1 weekly", CreateTask.frequencyTest(testStrings[7]));
-        assertEquals("2 yearly", CreateTask.frequencyTest(testStrings[8]));
-        assertEquals("3 monthly", CreateTask.frequencyTest(testStrings[9]));
+        assertEquals("3 daily", CreateTask.frequencyTest(frequencyStrings[0]));
+        assertEquals("1 daily", CreateTask.frequencyTest(frequencyStrings[1]));
+        assertEquals("1 weekly", CreateTask.frequencyTest(frequencyStrings[2]));
+        assertEquals("", CreateTask.frequencyTest(frequencyStrings[3]));
+        assertEquals("1 monthly", CreateTask.frequencyTest(frequencyStrings[4]));
+        assertEquals("2 weekly", CreateTask.frequencyTest(frequencyStrings[5]));
+        assertEquals("1 yearly", CreateTask.frequencyTest(frequencyStrings[6]));
+        assertEquals("1 weekly", CreateTask.frequencyTest(frequencyStrings[7]));
+        assertEquals("2 yearly", CreateTask.frequencyTest(frequencyStrings[8]));
+        assertEquals("3 monthly", CreateTask.frequencyTest(frequencyStrings[9]));
     }
 }
