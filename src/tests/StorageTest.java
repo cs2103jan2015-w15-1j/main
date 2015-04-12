dceebfea06d04a372df7cad6175041d5e9da015d
Ck91 <chenzhengkai91@gmail.com>
2015-04-12 23:31:39 +0800

    Update StorageTest to test the set method

--- src/tests/StorageTest.java
+++ src/tests/StorageTest.java
@@ -24,17 +24,21 @@ public class StorageTest {
     String defaultDirectory = "savefile.txt";
     String newDirectory = "saveRenamedFile.txt";
     String newDirectory2 = "memory.txt";
+    String newDirectory3 = "blank.txt";
     String SettingsDirectory = "settings.txt";
     String backupDirectory = "backup.txt";
     File defaultFile = new File(defaultDirectory);
     File backupFile = new File(backupDirectory);
     File testFile = new File(newDirectory);
     File testFile2 = new File(newDirectory2);
+    File testFile3 = new File(newDirectory3);
     File settingsFile = new File(SettingsDirectory);
     PrintWriter writer;
     BufferedReader reader;
 
+    // -------------------------------------------------------------------
     // test initializing settings file and moving save file
+    // -------------------------------------------------------------------
     @Test
     public void testSettings() {
         // delete any existing file before testing
@@ -49,8 +53,7 @@ public class StorageTest {
         // test moving save file
         assertEquals(true, defaultFile.exists());
         assertEquals(false, testFile.exists());
-        assertEquals(true,
-                test.moveSaveFileDirectory(newDirectory));
+        assertEquals(true, test.moveSaveFileDirectory(newDirectory));
         assertEquals(true, testFile.exists());
         assertEquals(false, defaultFile.exists());
 
@@ -63,8 +66,7 @@ public class StorageTest {
             e.printStackTrace();
         }
         assertEquals(false, testFile2.exists());
-        assertEquals(true,
-                test.moveSaveFileDirectory(newDirectory2));
+        assertEquals(true, test.moveSaveFileDirectory(newDirectory2));
         assertEquals(true, testFile2.exists());
         assertEquals(false, testFile.exists());
         try {
@@ -75,11 +77,22 @@ public class StorageTest {
             e.printStackTrace();
         }
         assertEquals(testData, text);
-        
+
+        // test setting another file as save file
+        assertEquals(false, test.setSaveFileDirectory(newDirectory3));
+        try {
+            testFile3.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertEquals(true, test.setSaveFileDirectory(newDirectory3));
+
         System.out.println("End of settings test");
     }
 
+    // -------------------------------------------------------------------
     // Test the write and read the task from the file
+    // -------------------------------------------------------------------
     @Test
     public void testWriteAndRead() {
         // data for testing
@@ -95,11 +108,13 @@ public class StorageTest {
         // read data from storage
         readData = test.readFile();
         compareData(tempData, readData);
-        
+
         System.out.println("End of write and read test");
     }
 
+    // -------------------------------------------------------------------
     // Test backup file
+    // -------------------------------------------------------------------
     @Test
     public void testBackup() {
         // data for testing
@@ -111,7 +126,7 @@ public class StorageTest {
 
         Storage test = Storage.getInstance();
         assertEquals(true, test.updateFiles(tempData));
-        
+
         // corrupting data
         try {
             writer = new PrintWriter(defaultFile, "UTF-8");
@@ -122,12 +137,12 @@ public class StorageTest {
         }
         readData = test.readFile();
         compareData(tempData, readData);
-        
+
         // if save file is not found
         defaultFile.delete();
         readData = test.readFile();
         compareData(tempData, readData);
-        
+
         // if backup is corrupted too, all data will be lost
         try {
             backupFile.setWritable(true);
@@ -140,15 +155,17 @@ public class StorageTest {
         }
         readData = test.readFile();
         assertEquals(true, readData.isEmpty());
-        
+
         // if backup file is not found
         readData = test.readFile();
         assertEquals(true, readData.isEmpty());
-        
+
         System.out.println("End of backup test");
     }
 
+    // -------------------------------------------------------------------
     // method to set-up the ArrayList
+    // -------------------------------------------------------------------
     private void createArrayListOfTask(String[] data, ArrayList<Task> tempData) {
         DateParser parser = DateParser.getInstance();
         for (String string : data) {
@@ -156,11 +173,14 @@ public class StorageTest {
             ArrayList<LocalDateTime> parsedDates = parser.getDates();
             String parsedWords = parser.getParsedWords();
             String nonParsedWords = parser.getNotParsedWords();
-            tempData.add(new Task(string, parsedDates, parsedWords, nonParsedWords));
+            tempData.add(new Task(string, parsedDates, parsedWords,
+                    nonParsedWords));
         }
     }
 
+    // -------------------------------------------------------------------
     // method to compare and check all variables of tasks
+    // -------------------------------------------------------------------
     private void compareData(ArrayList<Task> tempData, ArrayList<Task> readData) {
         for (int i = 0; i < readData.size(); i++) {
             assertEquals(tempData.get(i).isCompleted(), readData.get(i)
@@ -181,6 +201,7 @@ public class StorageTest {
     public void deleteAllFile() {
         testFile.delete();
         testFile2.delete();
+        testFile3.delete();
         defaultFile.delete();
         settingsFile.delete();
         backupFile.delete();
