aa00dc3b42445d2eeb426630c473d8998f2f6fb4
Ck91 <chenzhengkai91@gmail.com>
2015-04-13 04:36:42 +0800

    reduce scope of frequency test

--- src/tests/CreateTaskTest.java
+++ src/tests/CreateTaskTest.java
@@ -7,10 +7,8 @@ import org.junit.Test;
 
 //@author A0122393L
 public class CreateTaskTest {
-    CreateTask taskCreate = CreateTask.getInstance();
-    String[] frequencyStrings = { "every 3 day", "daily",
-            "weekly from today to 23 jul", "nothing", "monthly",
-            "every 2 weeks", "yearly", "weekly from tomorrow till 21 dec",
+    String[] frequencyStrings = { "everyday", "every 3 day", "daily",
+            "nothing", "monthly", "every 2 weeks", "yearly", "weekly",
             "every 2 year", "every 3 month" };
 
     // -------------------------------------------------------------------
@@ -18,9 +16,9 @@ public class CreateTaskTest {
     // -------------------------------------------------------------------
     @Test
     public void frequencyTest() {
-        assertEquals("3 daily", CreateTask.frequencyTest(frequencyStrings[0]));
-        assertEquals("1 daily", CreateTask.frequencyTest(frequencyStrings[1]));
-        assertEquals("1 weekly", CreateTask.frequencyTest(frequencyStrings[2]));
+        assertEquals("1 daily", CreateTask.frequencyTest(frequencyStrings[0]));
+        assertEquals("3 daily", CreateTask.frequencyTest(frequencyStrings[1]));
+        assertEquals("1 daily", CreateTask.frequencyTest(frequencyStrings[2]));
         assertEquals("", CreateTask.frequencyTest(frequencyStrings[3]));
         assertEquals("1 monthly", CreateTask.frequencyTest(frequencyStrings[4]));
         assertEquals("2 weekly", CreateTask.frequencyTest(frequencyStrings[5]));
